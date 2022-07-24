10935eb01b5cde10b91b5268176396139c508a9e
nithish125 <nithishs3620@gmail.com>
2022-07-24 19:25:37 +0530

    modified.

--- sffle.js
+++ sffle.js
@@ -1,57 +1,23 @@
-var nm1=document.getElementById("nm1")
-var nm2=document.getElementById("nm2")
-var nm3=document.getElementById("nm3")
-var nm4=document.getElementById("nm4")
-var nm5=document.getElementById("nm5")
-var sf=document.getElementById("sf")
-sf.addEventListener("click",sufle)
-r=["A","I","R","F","Y"]
+var nm1 = document.getElementById("nm1")
+var nm2 = document.getElementById("nm2")
+var nm3 = document.getElementById("nm3")
+var nm4 = document.getElementById("nm4")
+var nm5 = document.getElementById("nm5")
+var sf = document.getElementById("sf")
+sf.addEventListener("click", sufle)
+var r = ["A", "I", "R", "F", "Y"]
+var rr = [nm1, nm2, nm3, nm4, nm5]
 
-function sufle(){
-    var a=Math.floor((Math.random())*(r.length))
-    var s=Math.floor((Math.random())*(r.length))
-    var b=Math.floor((Math.random())*(r.length))
-    var c=Math.floor((Math.random())*(r.length))
-    var d=Math.floor((Math.random())*(r.length))
-   // var previous=a
+function sufle() {
+    rr.forEach((b)=>{
     
-    
-    
-        
-    function toFindDuplicates() {
-        let arry = [200,100,210,230,20];
-        let resultToReturn = false;
-        // call some function with callback function as argument
-        resultToReturn = arry.some((element, index) => {
-            return arry.indexOf(element) !== index
-        });
-        if (resultToReturn) {
-            while(resultToReturn = false){
-            console.log('Duplicate elements exist');
-             a=Math.floor((Math.random())*(r.length))
-             s=Math.floor((Math.random())*(r.length))
-             b=Math.floor((Math.random())*(r.length))
-             c=Math.floor((Math.random())*(r.length))
-             d=Math.floor((Math.random())*(r.length))
-            }
-            }
-            else {
-                nm1.innerText=r[a];
-                nm2.innerText=r[s];
-                nm3.innerText=r[b];
-                nm4.innerText=r[c];
-                nm5.innerText=r[d];
-                
-                }
-            }
-    
-            toFindDuplicates()
-    
-
-
-    // nm3.style.setProperty("color",r[s] );
-    console.log(s)
-    //console.log(Math.floor(Math.random()*4))
+      var  num = Math.floor(Math.random() * r.length)
+        b.innerHTML =r[num]
+        r.splice(num,1)  
+        console.log(b)
+        console.log (r[0])
+        //console.log( rr[num])
+    })
 }
-
+    
 

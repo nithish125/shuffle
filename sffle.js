a4673d0efa1617f643915e1c6e6683d0ec6f04ac
nithish125 <nithishs3620@gmail.com>
2022-07-27 17:49:54 +0530

    modified.

--- sffle.js
+++ sffle.js
@@ -1,15 +1,40 @@
 function load(){
   let b=document.querySelectorAll('#niuk');
-  let x="NIUK";
+  let x="NIGK";
   let y=x.split('');
   console.log(y);
+  var len=0
+  var d=document.querySelector(".num")
+  var sub=document.querySelector("input")
+  var balance=document.querySelector(".balance")
+  console.log(sub)
+  var emp=["KING","INK","IN","GIN","KIG","GK"]
   b.forEach((a)=>{
       let ran=Math.floor(Math.random()*y.length);
       a.innerHTML=y[ran];
       y.splice(ran,1);
-      console.log(ran);
+      
+      a.addEventListener("click",vl)
+      function vl(){
+        var s=this.innerHTML;
+         d.append(s)
+        console.log(d.innerHTML);
+        
+      }
      
   })
+  sub.addEventListener("click",add)
+  function add(){
+    var c=emp.includes(d.innerHTML)
+    console.log(emp.includes(d.innerHTML))
+    if(c==true){
+       
+     balance.innerHTML= emp.length-- 
+      console.log(emp.length)
+    }
+    d.innerHTML='';
+  }
 }
+
     
 

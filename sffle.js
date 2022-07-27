b3bb98862116152a3078bd543e293f73604ffa5b
nithish125 <nithishs3620@gmail.com>
2022-07-27 11:20:12 +0530

    modified.

--- sffle.js
+++ sffle.js
@@ -1,23 +1,15 @@
-var nm1 = document.getElementById("nm1")
-var nm2 = document.getElementById("nm2")
-var nm3 = document.getElementById("nm3")
-var nm4 = document.getElementById("nm4")
-var nm5 = document.getElementById("nm5")
-var sf = document.getElementById("sf")
-sf.addEventListener("click", sufle)
-var r = ["A", "I", "R", "F", "Y"]
-var rr = [nm1, nm2, nm3, nm4, nm5]
-
-function sufle() {
-    rr.forEach((b)=>{
-    
-      var  num = Math.floor(Math.random() * r.length)
-        b.innerHTML =r[num]
-        r.splice(num,1)  
-        console.log(b)
-        console.log (r[0])
-        console.log( rr[num])
-    })
-  }
+function load(){
+  let b=document.querySelectorAll('#niuk');
+  let x="NIUK";
+  let y=x.split('');
+  console.log(y);
+  b.forEach((a)=>{
+      let ran=Math.floor(Math.random()*y.length);
+      a.innerHTML=y[ran];
+      y.splice(ran,1);
+      console.log(ran);
+     
+  })
+}
     
 
